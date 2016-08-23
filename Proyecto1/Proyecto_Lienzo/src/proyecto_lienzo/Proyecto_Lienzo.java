c924349b6e0fb6dbf386169176e2df43ee016879
jerduar <jerduar@gmail.com>
2016-08-23 01:22:24 -0600

    Iniciando Proyecto 1

--- Proyecto1/Proyecto_Lienzo/src/proyecto_lienzo/Proyecto_Lienzo.java
+++ Proyecto1/Proyecto_Lienzo/src/proyecto_lienzo/Proyecto_Lienzo.java
@@ -15,7 +15,9 @@ public class Proyecto_Lienzo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        LienzoEdit nuevo = new LienzoEdit();
+        nuevo.setVisible(true);
+        nuevo.setLocationRelativeTo(null);
     }
     
 }
